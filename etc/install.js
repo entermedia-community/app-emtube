df7a46edd8852e631fc21e1c2e75f7a4ff7dda34
cburkey <cburkey@entermediasoftware.com>
2011-07-18 09:38:10 -0400

    Michael Tweak
    Change-Id: Icdc048c423b47975b632a27fb0a08b74c087177b

--- etc/install.js
+++ etc/install.js
@@ -3,24 +3,26 @@ importPackage( Packages.java.util );
 importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
-var zip = "http://dev.entermediasoftware.com/jenkins/job/app-emtube/lastSuccessfulBuild/artifact/deploy/app-emtube.zip";
+var appname = "emtube";
+
+var zip = "http://dev.entermediasoftware.com/jenkins/job/app-" + appname + "/lastSuccessfulBuild/artifact/deploy/app-" + appname + ".zip";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
-var eml = root + "/eml";
+var eml = root + "/"  + appname;
 var tmp = root + "/WEB-INF/tmp";
 
 log.add("1. GET THE LATEST ZIP FILE");
 var downloader = new Downloader();
-downloader.download( war, tmp + "/emtube.zip");
+downloader.download( war, tmp + "/app.zip");
 
 log.add("2. UNZIP WAR FILE");
 var unziper = new ZipUtil();
-unziper.unzip(  tmp + "/emtube.zip",  tmp );
+unziper.unzip(  tmp + "/app.zip",  tmp );
 
 var files = new FileUtils();
 log.add("4. UPGRADE BASE DIR");
-files.deleteAll( root + "/emtube");
-files.copyFiles( tmp + "/emtube", root);
+files.deleteAll( root + "/" + appname);
+files.copyFiles( tmp + "/" + appname, root);
 
 log.add("5. CLEAN UP");
 files.deleteAll(tmp);
